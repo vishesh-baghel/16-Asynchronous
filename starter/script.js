f315c56e529afeccfc42beded4f319d93dfef0e8
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-07-12 15:20:41 +0530

    made the first Ajax call

--- starter/script.js
+++ starter/script.js
@@ -4,23 +4,34 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/india');
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 
-request.send();
+  request.send();
 
-request.addEventListener('load', function () {
-  const [data] = JSON.parse(this.responseText);
-  console.log(data);
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-  const html = `<article class="country">
-  <img class="country__img" src="" />
+    const html = `<article class="country">
+  <img class="country__img" src="${data.flags}" />
   <div class="country__data">
-    <h3 class="country__name">COUNTRY</h3>
-    <h4 class="country__region">REGION</h4>
-    <p class="country__row"><span>👫</span>POP people</p>
-    <p class="country__row"><span>🗣️</span>LANG</p>
-    <p class="country__row"><span>💰</span>CUR</p>
+    <h3 class="country__name">${data.name}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages[0]}</p>
+    <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
   </div>
 </article>`;
-});
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('india');
+getCountryData('usa');

77de2cab448604495180c3f77d4efb8f94a7a7dc
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-07-14 17:00:04 +0530

    solved the problem of callback hell

--- starter/script.js
+++ starter/script.js
@@ -4,17 +4,8 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountryData = function (country) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
-
-  request.send();
-
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-
-    const html = `<article class="country">
+const renderCountry = function (data) {
+  const html = `<article class="country">
   <img class="country__img" src="${data.flags}" />
   <div class="country__data">
     <h3 class="country__name">${data.name}</h3>
@@ -26,12 +17,52 @@ const getCountryData = function (country) {
     <p class="country__row"><span>💰</span>${data.currencies[0]}</p>
   </div>
 </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
+const getCountryDataAndNeighbour = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+    renderCountry(data);
 
-    countriesContainer.style.opacity = 1;
+    const [neighbour] = data.borders;
+
+    if (!neighbour) return;
+
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      console.log(this.responseText);
+    });
   });
 };
 
+getCountryDataAndNeighbour('india');
+getCountryDataAndNeighbour('usa');
+
+const request = fetch(`https://restcountries.com/rest/v2/india`);
+console.log(request);
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/rest/v2/${country}`)
+    .then(function (response) {
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+};
+
 getCountryData('india');
-getCountryData('usa');
+
+btn.addEventListener('click', function () {
+  getCountryData('india');
+});

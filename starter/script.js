e85c5bea1d41d6bcdff8dc18b5e20e125608e56b
Vishesh Baghel <71141511+vishesh-baghel@users.noreply.github.com>
2022-07-18 15:54:11 +0530

    promisified some APIs

--- starter/script.js
+++ starter/script.js
@@ -50,15 +50,25 @@ getCountryDataAndNeighbour('usa');
 const request = fetch(`https://restcountries.com/rest/v2/india`);
 console.log(request);
 
+const getJSON = function (url, errorMsg = `Something went wrong`) {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+  });
+};
+
 const getCountryData = function (country) {
   fetch(`https://restcountries.com/rest/v2/${country}`)
     .then(function (response) {
       console.log(response);
+
+      if (!response.ok) throw new Error('Country not found');
+
       return response.json();
     })
     .then(function (data) {
       console.log(data);
-    });
+    })
+    .catch(err => alert(err));
 };
 
 getCountryData('india');
@@ -66,3 +76,45 @@ getCountryData('india');
 btn.addEventListener('click', function () {
   getCountryData('india');
 });
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+  countriesContainer.style.opacity = 1;
+};
+
+console.log('Test start');
+setTimeout(() => console.log(`0 sec timer`), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+console.log(`Test end`);
+
+Promise.resolve(`Resolved promise 2`).then(res => {
+  for (let i = 0; i < 100000; i++) {}
+  console.log(res);
+});
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve(`You Win`);
+    } else {
+      reject(`You lost your money`);
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.log(err));
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log(`I waited for 2 seconds`);
+    return wait(1);
+  })
+  .then(() => console.log(`I waited for 1 second`));
+
+navigator.geolocation.getCurrentPosition(position => console.log());
